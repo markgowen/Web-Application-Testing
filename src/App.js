f4694592ef3ce06b26623207bbaa83f540199c18
Mark Gowen <mark.gowen78@gmail.com>
2019-10-03 12:44:23 -0700

    Updated App.js, App.test.js
    Added initial styling with @material-ui to App.js
    Defined initial rules for testing to App.test.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  toolbar: {
+    backgroundColor: '#092668',
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
+    <div className={classes.root}>
+      <AppBar position='static'>
+        <Toolbar className={classes.toolbar} variant='dense'>
+          <Typography variant='h6' color='inherit'>
+            Pirates vs Cardinals
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      {/* Display Goes Here */}
+      {/* Dashboard Goes Here */}
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from '@testing-library/react';
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(<App />);
+});
+
+test('balls and strikes reset to 0 when a player reaches 3 strikes or 4 balls', () => {
+  expect('function().toBe()');
+});
+
+test('balls and strikes reset to 0 when a hit is recorded', () => {
+  expect('function().toBe()');
+});
+
+test('a foul increases strikes up to 2. With 2 strikes a foul has no effect, count stays at 2', () => {
+  expect('function().toBe()');
+  expect('function().not.toBe()');
 });
